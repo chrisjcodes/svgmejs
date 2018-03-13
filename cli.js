b76462b0d2f9f336574cc2e993a9c6b4366c6d5f
Chris Johnson <chris.johnson@madeinhaus.com>
2018-03-12 22:13:31 -0700

    Format keys in camelCase

--- cli.js
+++ cli.js
@@ -2,16 +2,19 @@
 const program = require('commander');
 const fs = require('fs');
 const glob = require('glob');
+const _camelCase = require('lodash/camelCase');
 
 const handleToJSON = (inputPath, outputPath) =>
     glob(`${inputPath}/*.svg`, (er, paths) => {
         const output = {};
 
         paths.map(pathName => {
-            const filename = pathName
+            let filename = pathName
                 .split('/')
                 .pop()
                 .replace('.svg', '');
+
+            filename = _camelCase(filename);
 
             let data = fs.readFileSync(pathName, 'utf8');
             data = data.replace(/"/g, "'").replace(/\n/g, '');
